e3e24391f50dee933ce74330d24d53f81d7e0c64
조민수 <40791869+eovywjr1@users.noreply.github.com>
2021-09-05 13:57:02 +0900

    0905
    player 이동 구현
    시작, 종료 버튼 구현

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float horizontal, vertical;
+    public float moveSpeed;
+
+    public bool isHorizentalDown, isVerticalDown;
+
+    public Vector2 moveDireciton;
+
+    Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MoveDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = moveDireciton * moveSpeed;
+    }
+
+    //위, 아래 방향 설정
+    void MoveDirection()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Horizontal"))
+            isHorizentalDown = true;
+        else if (Input.GetButtonUp("Horizontal"))
+            isHorizentalDown = false;
+        if (Input.GetButtonDown("Vertical"))
+            isVerticalDown = true;
+        else if (Input.GetButtonUp("Vertical"))
+            isVerticalDown = false;
+
+        if (isHorizentalDown)
+            moveDireciton = new Vector2(horizontal, 0);
+        else if (isVerticalDown)
+            moveDireciton = new Vector2(0, vertical);
+        else
+            moveDireciton = new Vector2(0, 0);
+    }
+}
