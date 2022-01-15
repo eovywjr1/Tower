ff5e84acf70b20dea2c8dd7b43cdaf43b4cd66c6
조민수 <40791869+eovywjr1@users.noreply.github.com>
2022-01-15 20:37:25 +0900

    0115_1
    노트북
    불 콜라이더 수정 중

--- Assets/Scripts/DragonScript.cs
+++ Assets/Scripts/DragonScript.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class DragonScript : BossBaseScript
 {
-    int dashSpeed = 5;
+    int dashSpeed = 5, dashDirection;
     bool isDash;
+    public Vector3 originPosition;
 
     public GameObject fire, fireGround, fireGroundPrefab;
     public LineRenderer fireGroundLineRenderer;
+    public BoxCollider2D fireGroundBoxCollider;
 
     private void Start()
     {
@@ -29,11 +31,24 @@ public class DragonScript : BossBaseScript
     
     void PrePareDash()
     {
-        playerPosition = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+        //4방향 랜덤
+        dashDirection = Random.Range(0, 2);
+        switch (dashDirection)
+        {
+            case 0:
+                playerPosition = new Vector2(Random.Range(-20, 20), transform.position.y);
+                break;
+            case 1:
+                playerPosition = new Vector2(transform.position.x, Random.Range(-20, 20));
+                break;
+        }
 
         fireGround = Instantiate(fireGroundPrefab);
         fireGroundLineRenderer = fireGround.GetComponent<LineRenderer>();
-        fireGroundLineRenderer.SetPosition(0, transform.position);
+        fireGroundBoxCollider = fireGround.GetComponent<BoxCollider2D>();
+
+        originPosition = transform.position;
+        fireGroundLineRenderer.SetPosition(0, originPosition);
 
         isDash = true;
     }
@@ -44,6 +59,13 @@ public class DragonScript : BossBaseScript
         transform.position = Vector2.MoveTowards(transform.position, playerPosition, Time.deltaTime * dashSpeed);
 
         fireGroundLineRenderer.SetPosition(1, transform.position);
+
+        //boxCollider 크기 및 위치 수정
+        Vector3 center = (transform.position - originPosition) / 2;
+
+        fireGroundBoxCollider.offset = center;
+        fireGroundBoxCollider.size = new Vector2(Mathf.Abs(center.x - transform.position.x), Mathf.Abs(center.y - transform.position.y));
+
     }
 
     void EndDash()
